419254b58e3388fef8852645be33015afcbba640
AlexKazmirchuk <zedomo@mail.ru>
2016-10-16 13:39:11 +0300

    added code for building and drawing random floor(updated)

--- app/src/main/java/com/alexkaz/zigzag/game_components/Floor.java
+++ app/src/main/java/com/alexkaz/zigzag/game_components/Floor.java
@@ -1,7 +1,6 @@
 package com.alexkaz.zigzag.game_components;
 
 import android.graphics.Canvas;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -9,53 +8,59 @@ import java.util.Random;
 public class Floor {
 
     private ArrayList<FloorElement> elementList;
+    private Random rand;
     private int x;
     private int y;
     private int translationX;
     private int translationY;
     private int elemCount;
     private int lastX;
+    private int leftBorder;
+    private int rightBorder;
 
     public Floor() {
+        elementList = new ArrayList<>();
+        rand = new Random();
+
         x = 240;
         y = 500;
         translationX = 40;
         translationY = 20;
         elemCount = 0;
         lastX = x;
-        elementList = new ArrayList<>();
+        leftBorder = 0;
+        rightBorder = 480;
+
         buildRandomFloor();
     }
 
     private void buildRandomFloor(){
         // записувати в ліст буліанів кожен шаг , по якому і визначатимемо колізію
-        elementList.add(new FloorElement(x,y));
-        elemCount = elementList.size();
-        int leftBorder = 0;
-        int rightBorder = 480;
-
-        Random random = new Random();
         for (int i = 0; i < 20; i++) {
-            int toLeftOrRight = random.nextInt(100);
-            if (toLeftOrRight >= 50){
-                if (lastX - translationX > leftBorder){
-                    elementList.add(new FloorElement(lastX - translationX, y - elemCount*translationY));
-                    lastX = lastX - translationX;
-                } else {
-                    elementList.add(new FloorElement(lastX + translationX, y - elemCount*translationY));
-                    lastX = lastX + translationX;
-                }
-            } else {
-                if (lastX + translationX*2 < rightBorder){
-                    elementList.add(new FloorElement(lastX + translationX, y - elemCount*translationY));
-                    lastX = lastX + translationX;
-                } else {
-                    elementList.add(new FloorElement(lastX - translationX, y - elemCount*translationY));
-                    lastX = lastX - translationX;
-                }
-            }
-            elemCount = elementList.size();
+            addFloorElem();
         }
+    }
+
+    public void addFloorElem(){
+        int toLeftOrRight = rand.nextInt(100);
+        if (toLeftOrRight >= 50){
+            if (lastX - translationX > leftBorder){
+                elementList.add(new FloorElement(lastX - translationX, y - elemCount*translationY));
+                lastX = lastX - translationX;
+            } else {
+                elementList.add(new FloorElement(lastX + translationX, y - elemCount*translationY));
+                lastX = lastX + translationX;
+            }
+        } else {
+            if (lastX + translationX*2 < rightBorder){
+                elementList.add(new FloorElement(lastX + translationX, y - elemCount*translationY));
+                lastX = lastX + translationX;
+            } else {
+                elementList.add(new FloorElement(lastX - translationX, y - elemCount*translationY));
+                lastX = lastX - translationX;
+            }
+        }
+        elemCount = elementList.size();
     }
 
     public void draw(Canvas canvas){
